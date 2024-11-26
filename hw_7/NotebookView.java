71d1e182cb9ccf91810bff3e690da27edd35f357
Roman Rzyanin <roman@rzyanin.ru>
2024-11-26 21:41:51 +0300

    OOP_hw_7_2024.11.26_v_1.1

--- hw_7/NotebookView.java
+++ hw_7/NotebookView.java
@@ -1,9 +1,10 @@
 import java.time.LocalDateTime;
 import java.util.List;
+
 public interface NotebookView {
-void showNotes(List<Note> notes);
-void showMessage(String message);
-LocalDateTime getDateTimeInput();
-String getDescriptionInput();
-String getFileNameInput();
+    void showNotes(List<Note> notes);
+    void showMessage(String message);
+    LocalDateTime getDateTimeInput();
+    String getDescriptionInput();
+    String getFileNameInput();
 }
