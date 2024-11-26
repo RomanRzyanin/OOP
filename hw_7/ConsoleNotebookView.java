94ed013497ea45a2cf9115c47d1bb55d0f81f0ed
Roman Rzyanin <roman@rzyanin.ru>
2024-11-26 21:25:11 +0300

    OOP_hw_7_2024.11.26_v_1

--- hw_7/ConsoleNotebookView.java
+++ hw_7/ConsoleNotebookView.java
@@ -2,38 +2,42 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
+
 public class ConsoleNotebookView implements NotebookView {
-private Scanner scanner = new Scanner(System.in);
-@Override
-public void showNotes(List<Note> notes) {
-if (notes.isEmpty()) {
-System.out.println("No notes found.");
-} else {
-for (Note note : notes) {
-System.out.println(note);
-}
-}
-}
-@Override
-public void showMessage(String message) {
-System.out.println(message);
-}
-@Override
-public LocalDateTime getDateTimeInput() {
-System.out.println("Enter date and time (yyyy-MM-dd'T'HH:mm):");
-String input = scanner.nextLine();
-DateTimeFormatter formatter =
-DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-return LocalDateTime.parse(input, formatter);
-}
-@Override
-public String getDescriptionInput() {
-System.out.println("Enter note description:");
-return scanner.nextLine();
-}
-@Override
-public String getFileNameInput() {
-System.out.println("Enter file name:");
-return scanner.nextLine();
-}
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void showNotes(List<Note> notes) {
+        if (notes.isEmpty()) {
+            System.out.println("Заметки не найдены");
+        } else {
+            for (Note note : notes) {
+            System.out.println(note);
+        }
+        }
+    }
+
+    @Override
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public LocalDateTime getDateTimeInput() {
+        System.out.println("Введите дату и время в формате:(yyyy-MM-dd'T'HH:mm):");
+        String input = scanner.nextLine();
+        DateTimeFormatter formatter =
+        DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        return LocalDateTime.parse(input, formatter);
+    }
+    @Override
+    public String getDescriptionInput() {
+        System.out.println("Введите описание заметки:");
+        return scanner.nextLine();
+    }
+    @Override
+    public String getFileNameInput() {
+        System.out.println("Введите название файла:");
+        return scanner.nextLine();
+    }
 }
--- hw_7/Main.java
+++ hw_7/Main.java
@@ -1,43 +1,43 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
 public class Main {
-public static void main(String[] args) {
-Notebook model = new Notebook();
-NotebookView view = new ConsoleNotebookView();
-NotebookPresenter presenter = new NotebookPresenter(model,
-view);
-Scanner scanner = new Scanner(System.in);
-while (true) {
-System.out.println("1. Add Note");
-System.out.println("2. Show Notes for Day");
-System.out.println("3. Show Notes for Week");
-System.out.println("4. Save Notes");
-System.out.println("5. Load Notes");
-System.out.println("6. Exit");
-int choice = scanner.nextInt();
-scanner.nextLine(); // Consume newline
-switch (choice) {
-case 1:
-presenter.addNote();
-break;
-case 2:
-presenter.showNotesForDay();
-break;
-case 3:
-presenter.showNotesForWeek();
-break;
-case 4:
-presenter.saveNotes();
-break;
-case 5:
-presenter.loadNotes();
-break;
-case 6:
-return;
-default:
-System.out.println("Invalid choice");
-}
-}
-}
+    public static void main(String[] args) {
+        Notebook model = new Notebook();
+        NotebookView view = new ConsoleNotebookView();
+        NotebookPresenter presenter = new NotebookPresenter(model, view);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Добавить заметку");
+            System.out.println("2. Показать заметки за день");
+            System.out.println("3. Показать заметки за неделю");
+            System.out.println("4. Сохранить заметки");
+            System.out.println("5. Выгрузить заметки");
+            System.out.println("6. Выход");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            switch (choice) {
+            case 1:
+                presenter.addNote();
+                break;
+            case 2:
+                presenter.showNotesForDay();
+                break;
+            case 3:
+                presenter.showNotesForWeek();
+                break;
+            case 4:
+                presenter.saveNotes();
+                break;
+            case 5:
+                presenter.loadNotes();
+                break;
+            case 6:
+                return;
+            default:
+            System.out.println("Вы ошиблись с выбором. Попробуйте сначала");
+            }
+        }
+    }
 }
